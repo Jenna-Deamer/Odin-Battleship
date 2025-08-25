22bab729348ae72c44137fec524408ad03f7622d
Jenna Deamer <jennadeamer@gmail.com>
2025-08-25 17:12:21 -0400

    Project setup with jest, Add CreateShip factory

---
+++ ship.js
@@ -0,0 +1,19 @@
+function createShip(length){
+
+if(length <=0){
+    console.warn('Error - ship length must be positive');
+}
+
+let hits = 0;
+
+function hit(){
+    
+}
+
+function isSunk(){
+
+}
+
+return{length, isSunk, hit}
+}
+module.exports = createShip;
