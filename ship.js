176346bbedeec1a6a89d14dcf5f8d77feade77c2
Jenna Deamer <jennadeamer@gmail.com>
2025-08-25 19:59:41 -0400

    Add hit function to ship factory that checks if the attack loc matches any ship locs, Add test for hit check

--- ship.js
+++ ship.js
@@ -1,19 +1,24 @@
-function createShip(length){
+function createShip(length) {
+	if (length <= 0) {
+		console.warn('Error - ship length must be positive');
+	}
 
-if(length <=0){
-    console.warn('Error - ship length must be positive');
+	let hits = 0;
+
+	function hit(shipLocations, attackLocation) {
+		for (let i = 0; i < shipLocations.length; i++) {
+			if (shipLocations[i][0] === attackLocation[0] && shipLocations[i][1] === attackLocation[1]) {
+				hit++;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function isSunk() {}
+
+	return { length, isSunk, hit };
 }
 
-let hits = 0;
-
-function hit(){
-    
-}
-
-function isSunk(){
-
-}
-
-return{length, isSunk, hit}
-}
 module.exports = createShip;
+
--- tests.test.js
+++ tests.test.js
@@ -1,2 +1,6 @@
-const ship = require('./ship');
+const createShip = require('./ship');
 
+const ship = createShip(3);
+
+test(`Checks player's ship locations against chosen square to determine if ship was hit`, () =>{
+   expect(ship.hit([[2,2],[2,3],[2,4]], [2,2])).toBeTruthy();})
