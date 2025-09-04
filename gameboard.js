803dc420ab28c97f009b07cfd9c76b0760d66f34
Jenna Deamer <jennadeamer@gmail.com>
2025-09-04 19:27:29 -0400

    Add ship placing logic Refine place ship test

--- gameboard.js
+++ gameboard.js
@@ -1,17 +1,27 @@
 function gameboard() {
-let ships = [];
-let hitSquares = []
-let missedSquares = []
+	let ships = [];
+	let hitSquares = [];
+	let missedSquares = [];
 
-    function placeShip(startPos,endPos, direction, ship){
-        if(direction === 'horizontal'){
-            
-        }else if(direction === 'vertical'){
+	function placeShip(startPos, direction, ship) {
+		let positions = [];
+		if (direction === 'horizontal') {
+			// increment col
+			for (let i = 0; i < ship.length; i++) {
+				positions.push([startPos[0], startPos[1] + i]);
+			}
+			// increment row
+		} else if (direction === 'vertical') {
+			for (let i = 0; i < ship.length; i++) {
+				positions.push([startPos[0] + i, startPos[1]]);
+			}
+		}
 
-        }
-    }
+		// push ship with occupied tiles
+		ships.push({ ship, positions });
+	}
 
-    return{placeShip}
+	return { placeShip, ships };
 }
 
 module.exports = gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,10 +1,11 @@
 const gameboard = require('../gameboard');
 const ship = require('../ship');
 
-const shipOne = ship(3);
-const board = gameboard();
-const direction = 'horizontal';
 
 test('Places a ship at provided location if it is in bounds', () => {
-	expect(board.placeShip([0, 0], [0, 3], direction, shipOne)).toBeTruthy();
+	const shipOne = ship(3);
+	const board = gameboard();
+	board.placeShip([0,0], 'horizontal', shipOne);
+
+	expect(board.ships[0].positions).toEqual([[0,0], [0,1], [0,2]]);
 });
